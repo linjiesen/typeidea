0ce4895664e562c01c84354a7634745c1babeda0
Alroy <sqrtln@163.com>
2020-06-02 00:27:54 +0800

    取消劃分settings文件

--- typeidea/typeidea/settings.py
+++ typeidea/typeidea/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd%-z4w5sat!k0#fh5uv$c$7drm(5%$v)#t0j-pcrs79u)*k2g)'
+SECRET_KEY = 'd%-z4w5sat!k0#fh5uv$c$7drm(5%$v)#t0j-pcrs79u)*k2g).'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,12 +77,16 @@ WSGI_APPLICATION = 'typeidea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Typeidea',
+        'USER': 'root',
+        'PASSWORD': 'kid1412',
+        'HOST': '127.0.0.1',
+    }
+}
+
 
 
 # Password validation
